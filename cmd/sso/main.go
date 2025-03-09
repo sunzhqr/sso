f5f7ad8cbe5951954d3bbfcf113561f92fc39daa
smyrzash <sanzhar_myrzash@mail.ru>
2025-03-09 21:05:47 +0500

    feat: logger's configuration

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,15 +3,53 @@ package main
 import (
 	"fmt"
 	"github.com/sunzhqr/sso/internal/config"
+	"github.com/sunzhqr/sso/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println("This is config:", cfg)
 
-	// TODO: initialize logger
+	logger := setupLogger(cfg.Env)
+	logger.Info("starting application", slog.Any("config", cfg))
 
 	// TODO: initialize app
 
 	// TODO: launch gPRC-server of the app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOpts{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.22.3
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/sunzhqr/protos v1.1.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
